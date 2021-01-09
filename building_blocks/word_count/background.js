0b3a4e31120733dd260feeba6caad5722a14a74c
Haydn Cheong <haydncheong@Haydns-MacBook-Pro.local>
2021-01-09 14:07:55 +0000

    Initial commit - a sample created to count specified words of all tabs.

---
+++ building_blocks/word_count/background.js
@@ -0,0 +1,8 @@
+window.words = {}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    window.words[request.url] = request.count
+})
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({url: 'popup.html'})
+})
